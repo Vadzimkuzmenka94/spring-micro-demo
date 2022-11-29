92bcbc22cb056c67e14128f7656e5abc42577af6
Vadzim Kuzmenka <vadzimkuzmenka@mail.ru>
2022-11-29 18:21:24 +0400

    1

---
+++ eureka-entity/src/main/java/com/example/eurekaentity/EurekaEntityApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaEntityApplication.class, args);
+    }
+
+}
---
+++ eureka-entity/src/test/java/com/example/eurekaentity/EurekaEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
